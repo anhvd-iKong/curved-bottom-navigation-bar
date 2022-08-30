849a0c2214a4df292d4339039e3b4bbad96b4422
anhvd-iKong <112453263+anhvd-iKong@users.noreply.github.com>
2022-08-30 10:04:47 +0700

    Update Dot.tsx

--- src/curved/item/Dot.tsx
+++ src/curved/item/Dot.tsx
@@ -55,7 +55,7 @@ const DotComponent = (props: DotProps) => {
   const translateY = useInterpolate(
     progress,
     [0, 1],
-    [15 - barHeight, -(heightHole - HEIGHT_HOLE + 5)]
+    [15 - barHeight, -(heightHole - heightHole + 5)]
   );
 
   const opacity = useInterpolate(progress, [0, 1], [1, 1]);
